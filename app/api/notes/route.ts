98cab3cd3075b95c50e10ad4f08a93cafa899d60
Kvasko Anna <kvasko.anna@ukr.net>
2025-11-12 16:16:06 +0200

    hw-9

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -6,15 +6,15 @@ import { logErrorResponse } from "@/lib/utils/logErrorResponse";
 
 export async function GET(request: NextRequest) {
   try {
-    const cookieStore = await cookies();
+    const cookieStore = cookies();
     const search = request.nextUrl.searchParams.get("search") ?? "";
     const page = Number(request.nextUrl.searchParams.get("page") ?? 1);
     const rawTag = request.nextUrl.searchParams.get("tag") ?? "";
     const tag = rawTag === "All" ? "" : rawTag;
 
-    const res = await api("/notes", {
+    const { data, status } = await api.get("/notes", {
       params: {
-        ...(search !== "" && { search }),
+        ...(search && { search }),
         page,
         perPage: 12,
         ...(tag && { tag }),
@@ -24,13 +24,13 @@ export async function GET(request: NextRequest) {
       },
     });
 
-    return NextResponse.json(res.data, { status: res.status });
+    return NextResponse.json(data, { status });
   } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
       return NextResponse.json(
         { error: error.message, response: error.response?.data },
-        { status: error.status }
+        { status: error.response?.status ?? 500 }
       );
     }
     logErrorResponse({ message: (error as Error).message });
@@ -43,24 +43,23 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   try {
-    const cookieStore = await cookies();
-
+    const cookieStore = cookies();
     const body = await request.json();
 
-    const res = await api.post("/notes", body, {
+    const { data, status } = await api.post("/notes", body, {
       headers: {
         Cookie: cookieStore.toString(),
         "Content-Type": "application/json",
       },
     });
 
-    return NextResponse.json(res.data, { status: res.status });
+    return NextResponse.json(data, { status });
   } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
       return NextResponse.json(
         { error: error.message, response: error.response?.data },
-        { status: error.status }
+        { status: error.response?.status ?? 500 }
       );
     }
     logErrorResponse({ message: (error as Error).message });
